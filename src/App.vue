4a32ebaa3f00f33f856c6eaf9d4ae81abb353a7f
Евгений Чупеко <echupeko@gmail.com>
2022-10-24 18:03:02 +0700

    Added typescript and vuex store

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
     <router-view />
     <div>
       <HomePage/>
-      <AboutPage :title="'About'"/>
+      <AboutPage/>
     </div>
   </div>
 </template>
--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        },
+        exclude: /node_modules/,
+      },
+      {
         test: /\.vue$/,
         loader: 'vue-loader',
       },
